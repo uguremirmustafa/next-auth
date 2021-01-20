197123c51afa8a6a5ec83d9c6e68251bee8485f7
uguremirmustafa <uguremirmustafa@gmail.com>
2021-01-20 14:14:42 +0300

    next-auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '@styles/globals.css'
+/** @format */
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Provider } from 'next-auth/client';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
-
-export default Application
